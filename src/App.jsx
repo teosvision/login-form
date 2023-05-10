a43d07e3738fd93014d343dd6964952d3790d7b4
teosvision <mateovelo99@gmail.com>
2023-05-10 10:25:07 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="flex justify-center h-screen items-center">Hello</div>
+    </div>
+  );
+}
+
+export default App;
